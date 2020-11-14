3148a8991acb0e9629fe13711896f62e3be1c626
Yoshiki Obinata <mqcmd196@hotmail.co.jp>
2020-11-14 15:46:55 +0900

    add .gitignore_global

--- setup.sh
+++ setup.sh
@@ -2,3 +2,5 @@
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.emacs ~/.emacs
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
+git config --global core.execludesFile ~/.gitignore_global
