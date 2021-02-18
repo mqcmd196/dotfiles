a842a1f9776245a57e5dc4f9679cfc101e65b289
Yoshiki Obinata <mqcmd196@hotmail.co.jp>
2021-02-18 13:18:31 +0900

    fix typo

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.emacs ~/.emasc
+ln -sf ~/dotfiles/.emacs ~/.emacs
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
