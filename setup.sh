5101dfcb02ea572956b4e7b3efd0338a6f7258f0
Yoshiki Obinata <mqcmd196@hotmail.co.jp>
2021-04-07 09:27:08 +0900

    add percol.d

--- setup.sh
+++ setup.sh
@@ -5,4 +5,6 @@ ln -sf ~/dotfiles/.emacs ~/.emacs
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+mkdir ~/.percol.d
+ln -sf ~/dotfiles/rc.py ~/.percol.d/rc.py
 git config --global core.execludesFile ~/.gitignore_global
