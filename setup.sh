b34d1fabd3b187cd071e99361d6baf77eb66e6df
Yoshiki Obinata <mqcmd196@hotmail.co.jp>
2021-04-17 22:49:18 +0900

    update setup.sh to ln .zshrc

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.emacs ~/.emacs
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 # percol
