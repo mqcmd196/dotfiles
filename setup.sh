7839e6ff75abb23596e368da13151b2b8efb1cab
Mujin <mujin@PF1GDP5M.mujin.co.jp>
2021-09-09 19:26:29 +0900

    rewrite dotfiles for setting up another hosts

--- setup.sh
+++ setup.sh
@@ -1,13 +1,61 @@
-#!/bin/sh
+#!/bin/bash
+SETUP_CONFIGDIR=$(cd $(dirname $0);pwd)
 
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.emacs ~/.emacs
-ln -sf ~/dotfiles/.bashrc ~/.bashrc
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
-ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
-# percol
-mkdir ~/.percol.d
-ln -sf ~/dotfiles/percol.zsh ~/percol.zsh
-ln -sf ~/dotfiles/rc.py ~/.percol.d/rc.py
-git config --global core.execludesFile ~/.gitignore_global
+EMACS_CONFIGDIR="${SETUP_CONFIGDIR}/emacs"
+VIM_CONFIGDIR="${SETUP_CONFIGDIR}/vim"
+
+PERCOL_CONFIGDIR="${SETUP_CONFIGDIR}/percol"
+
+BASH_CONFIGDIR="${SETUP_CONFIGDIR}/bash"
+ZSH_CONFIGDIR="${SETUP_CONFIGDIR}/zsh"
+
+TMUX_CONFIGDIR="${SETUP_CONFIGDIR}/tmux"
+
+GIT_CONFIGDIR="${SETUP_CONFIGDIR}/git"
+
+# editor settings
+setup_editor(){
+    echo "Setting up the editor configs..."
+    $EMACS_CONFIGDIR/setup.sh # emacs
+    ln -sf $VIM_CONFIGDIR/vimrc $HOME/.vimrc 	# vim
+    echo "Finished to set the editor configs"
+}
+
+# percol settings
+setup_percol(){
+    echo "Setting up the percol configs..."
+    $PERCOL_CONFIGDIR/setup.sh
+    echo "Finished to set the percol configs"
+}
+
+# shell settings
+setup_shell(){
+    echo "Setting up the shell configs..."
+    ln -sf $BASH_CONFIGDIR/bashrc $HOME/.bashrc
+    ln -sf $ZSH_CONFIGDIR/zshrc $HOME/.zshrc
+    echo "Finished to set the shell configs"
+}
+
+# tmux settings
+setup_tmux(){
+    echo "Setting up the tmux configs..."
+    $TMUX_CONFIGDIR/setup.sh
+    echo "Finished to set the tmux configs"
+}
+
+# git settings
+setup_git(){
+    echo "Setting up the git configs..."
+    $GIT_CONFIGDIR/setup.sh
+    echo "Finished to set the git configs"
+}
+
+main(){
+    setup_editor
+    setup_percol
+    setup_shell
+    setup_tmux
+    setup_git
+}
+
+main
