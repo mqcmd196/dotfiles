b8f3842a185d2fe5ebb717f2124a9fb632459df5
Yoshiki Obinata <mqcmd196@hotmail.co.jp>
2021-09-16 18:21:40 +0900

    add magit

--- emacs/init.el
+++ emacs/init.el
@@ -52,7 +52,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (ccls use-package company lsp-ui lsp-mode gnu-elpa-keyring-update))))
+    (magit ccls use-package company lsp-ui lsp-mode gnu-elpa-keyring-update))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
