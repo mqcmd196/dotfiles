d0f6c0819e1f6814623fa0b04b4524ea3b43aaab
Yoshiki Obinata <mqcmd196@hotmail.co.jp>
2022-01-27 12:35:35 +0900

    fix yasnippets, emacs. add non-sudoer

--- emacs/conf/21-snippets.el
+++ emacs/conf/21-snippets.el
@@ -1,1 +1,12 @@
- (yas-global-mode 1)
+(use-package yasnippet
+  :ensure t
+  :diminish yas-minor-mode
+  :bind (:map yas-minor-mode-map
+              ("C-x i i" . yas-insert-snippet)
+              ("C-x i n" . yas-new-snippet)
+              ("C-x i v" . yas-visit-snippet-file)
+              ("C-x i l" . yas-describe-tables)
+              ("C-x i g" . yas-reload-all))
+  :config
+  (yas-global-mode 1)
+  (setq yas-prompt-functions '(yas-ido-prompt)))
