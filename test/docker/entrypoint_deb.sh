ebe74988bfc8fd39bdf77caeec9658b90c5f0714
Yoshiki Obinata <mqcmd196@hotmail.co.jp>
2022-03-25 18:47:04 +0900

    fix setup.sh path, remove ccls submodule, fix entrypoint to install some packages

--- test/docker/entrypoint_deb.sh
+++ test/docker/entrypoint_deb.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-sudo apt update && apt upgrade -y
+sudo apt update && sudo apt upgrade -y
 sudo apt install -y --no-install-recommends \
     git \
     python3 \
+    python3-apt \
     python3-pip \
+    python3-distro \
     snapd \
-    && rm -rf /var/lib/apt/lists/*
-sudo pip3 install pyyaml distro
+    && sudo rm -rf /var/lib/apt/lists/*
+sudo pip3 install pyyaml
 
 ./setup
 ./test/test
