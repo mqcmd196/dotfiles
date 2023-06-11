5a717b3c6083de89b0660773b9f7d5b8fa21bac8
Yoshiki Obinata <mqcmd196@hotmail.co.jp>
2023-06-11 13:36:16 +0900

    [zsh] p10k fix

--- roles/zsh/config/rc.zsh
+++ roles/zsh/config/rc.zsh
@@ -16,7 +16,8 @@ export ZSH=$HOME/.oh-my-zsh
 # load a random theme each time oh-my-zsh is loaded, in which case,
 # to know which specific one was loaded, run: echo $RANDOM_THEME
 # See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
-ZSH_THEME="powerlevel10k/powerlevel10k"
+ZSH_CUSTOM=$HOME/.powerlevel10k
+ZSH_THEME="powerlevel10k"
 
 # Uncomment the following line to use case-sensitive completion.
 # CASE_SENSITIVE="true"
