fd54193f426165d0d00df3095b1716191a3462e7
Yoshiki Obinata <mqcmd196@hotmail.co.jp>
2023-11-24 15:10:50 +0900

    [emacs] add light version config. [zsh] add zsh alias

--- non-sudoer/emacs.el
+++ non-sudoer/emacs.el
@@ -9,9 +9,28 @@
 
 (setq-default tab-width 4 indent-tabs-mode nil)
 
+;; Insert parenthesis/brackets by pair
+(electric-pair-mode 1)
+
 ;; yaml-mode, nxml in ros
 (when (require 'yaml-mode nil t)
   (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode)))
-(when (require 'nxml-mode nil t)
-  (add-to-list 'auto-mode-alist '("\\.launch$" . nxml-mode))
-  (add-to-list 'auto-mode-alist '("\\.xml$" . nxml-mode)))
+(add-to-list 'auto-mode-alist '("\.launch$" . nxml-mode))
+(add-to-list 'auto-mode-alist '("\.test$" . nxml-mode))
+(add-to-list 'auto-mode-alist '("manifest.xml" . nxml-mode))
+(add-to-list 'auto-mode-alist '("\\.urdf" . xml-mode))
+(add-to-list 'auto-mode-alist '("\\.xacro" . xml-mode))
+(add-to-list 'auto-mode-alist '("\\.msg\\'" . gdb-script-mode))
+(add-to-list 'auto-mode-alist '("\\.srv\\'" . gdb-script-mode))
+(add-to-list 'auto-mode-alist '("\\.action\\'" . gdb-script-mode))
+(font-lock-add-keywords 'gdb-script-mode                        '(("\\<\\(bool\\|byte\\|int8\\|uint8\\|int16\\|uint16\\|int32\\|uint32\\|int64\\|uint64\\|float32\\|float64\\|string\\|time\\|duration\\)\\>" . font-lock-builtin-face)) 'set)
+
+;; do not copy when M-DEL and C-backspace
+(defun delete-word (arg)
+  (interactive "p")
+  (delete-region (point) (progn (forward-word arg) (point))))
+(defun backward-delete-word (arg)
+  (interactive "p")
+  (delete-word (- arg)))
+(global-set-key (read-kbd-macro "<M-DEL>") 'backward-delete-word) ;; not kill-ring with M-DEL
+(global-set-key (read-kbd-macro "<C-backspace>") 'delete-word) ;; not kill-ring with M-d
--- roles/zsh/config/alias.zsh
+++ roles/zsh/config/alias.zsh
@@ -2,27 +2,42 @@
 
 alias em='emacs'
 alias doom='~/.emacs.d/bin/doom'
+alias eml='emacs -q --load ~/.emacs-light.el'
 
 if type "batcat" > /dev/null 2>&1; then
     alias bat="batcat"
 fi
 
+# ls
 alias ls='ls --color=auto'
 alias ll='ls -alF'
 alias la='ls -A'
 alias l='ls -CF'
 
+# grep
 alias grep='grep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias egrep='egrep --color=auto'
 
-alias ip='grc ip'
-alias ifconfig='grc ifconfig'
-alias ping='grc ping'
-alias traceroute='grc traceroute'
-alias netstat='grc netstat'
-alias ps='grc ps'
-alias df='grc df'
+# colorize some commands output
+if type "grc" > /dev/null 2>&1; then
+    alias ip='grc ip'
+    alias ifconfig='grc ifconfig'
+    alias ping='grc ping'
+    alias traceroute='grc traceroute'
+    alias netstat='grc netstat'
+    alias ps='grc ps'
+    alias df='grc df'
+fi
+
+# git
+alias gfa='git fetch --all'
+alias gco='git checkout'
+alias ga='git add'
+alias gcm='git commit -m'
+alias gp='git push'
+alias gst='git status'
+alias gd='git diff'
 
 source /etc/lsb-release
 if [[ $DISTRIB_RELEASE == 22.04 ]] then
