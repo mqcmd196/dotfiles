a123dff2b3baab619bbfb25e622789332645d45e
Yoshiki Obinata <mqcmd196@hotmail.co.jp>
2023-12-23 14:39:55 +0900

    Fix copilot kbd. Use C-e or M-f

--- roles/emacs/doom/config/config.el
+++ roles/emacs/doom/config/config.el
@@ -154,10 +154,8 @@
 (use-package! copilot
   :hook (prog-mode . copilot-mode)
   :bind (:map copilot-completion-map
-              ("<tab>" . 'copilot-accept-completion)
-              ("TAB" . 'copilot-accept-completion)
-              ("C-TAB" . 'copilot-accept-completion-by-word)
-              ("C-<tab>" . 'copilot-accept-completion-by-word)))
+              ("C-e" . 'copilot-accept-completion)
+              ("M-f" . 'copilot-accept-completion-by-word)))
 
 ;; dap mode
 (after! dap-mode
