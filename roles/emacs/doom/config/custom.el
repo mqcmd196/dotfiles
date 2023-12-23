6f04a7460db7d3a361a0ce04a920fa15f4769dfc
Yoshiki Obinata <mqcmd196@hotmail.co.jp>
2023-12-23 19:14:06 +0900

    update for emacs29

--- roles/emacs/doom/config/custom.el
+++ roles/emacs/doom/config/custom.el
@@ -3,8 +3,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
  '(safe-local-variable-values '((encoding . utf-8)))
- '(warning-suppress-log-types '((doom-first-input-hook) (defvaralias))))
+ '(warning-suppress-log-types
+   '((emacs)
+     ((copilot copilot-no-mode-indent))
+     (doom-first-input-hook)
+     (defvaralias))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
