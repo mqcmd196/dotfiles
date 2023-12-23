2576d0de33393df7e3b9f694b8c3a2fcf2dac23c
Yoshiki Obinata <mqcmd196@hotmail.co.jp>
2023-12-23 15:08:16 +0900

    update theme

--- non-sudoer/themes/obinata-deeper-blue-theme.el
+++ non-sudoer/themes/obinata-deeper-blue-theme.el
@@ -11,7 +11,7 @@
  '(homoglyph ((((background dark)) (:foreground "cyan")) (((type pc)) (:foreground "magenta")) (t (:foreground "brown"))))
  '(minibuffer-prompt ((t (:foreground "CadetBlue1"))))
  '(highlight ((t (:background "DodgerBlue4"))))
- '(region ((t (:extend t :background "#103050"))))
+ '(region ((t (:extend t :background "royalblue4"))))
  '(shadow ((((class color grayscale) (min-colors 88) (background light)) (:foreground "grey50")) (((class color grayscale) (min-colors 88) (background dark)) (:foreground "grey70")) (((class color) (min-colors 8) (background light)) (:foreground "green")) (((class color) (min-colors 8) (background dark)) (:foreground "yellow"))))
  '(secondary-selection ((((class color) (min-colors 88) (background light)) (:extend t :background "yellow1")) (((class color) (min-colors 88) (background dark)) (:extend t :background "SkyBlue4")) (((class color) (min-colors 16) (background light)) (:extend t :background "yellow")) (((class color) (min-colors 16) (background dark)) (:extend t :background "SkyBlue4")) (((class color) (min-colors 8)) (:extend t :foreground "black" :background "cyan")) (t (:inverse-video t))))
  '(trailing-whitespace ((((class color) (background light)) (:background "red1")) (((class color) (background dark)) (:background "red1")) (t (:inverse-video t))))
