4d8042e24027f742b27ca92e141a11755c5466a1
Yoshiki Obinata <mqcmd196@hotmail.co.jp>
2025-11-13 13:33:54 +0900

    set CMAKE_EXPORT_COMPILE_COMMANDS=ON by default

--- roles/zsh/config/rc.zsh
+++ roles/zsh/config/rc.zsh
@@ -121,3 +121,6 @@ ZSH_HIGHLIGHT_STYLES[comment]=fg=245 # make comment out color lighter
 source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 ZSH_AUTOSUGGEST_STRATEGY=(completion history)
 ZSH_AUTOSUGGEST_USE_ASYNC=1 # avoid too slow completion
+
+# CMake options
+export CMAKE_EXPORT_COMPILE_COMMANDS=ON
