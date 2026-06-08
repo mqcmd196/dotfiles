0fce5d489fc861fc061b01c7cab9e17a9ee597c2
Yoshiki Obinata <mqcmd196@hotmail.co.jp>
2026-06-08 14:59:15 +0900

    enable ccache when cmake

--- roles/common/zsh/rc.zsh
+++ roles/common/zsh/rc.zsh
@@ -124,3 +124,5 @@ ZSH_AUTOSUGGEST_USE_ASYNC=1 # avoid too slow completion
 
 # CMake options
 export CMAKE_EXPORT_COMPILE_COMMANDS=ON
+export CMAKE_C_COMPILER_LAUNCHER=ccache
+export CMAKE_CXX_COMPILER_LAUNCHER=ccache
